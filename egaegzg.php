001d86a3402479018de4f5b3920bf95e9cc2da9e
chenow <antoine.cheneau@student-cs.fr>
2022-04-11 12:28:01 +0200

    remove test files

--- egaegzg.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-include_once 'includes/connexion_bdd.php';
-if (isset($_COOKIE["session_id_orgif"])) {
-    $sessionID = $_COOKIE["session_id_orgif"];
-    $sql = "SELECT carte1, carte2 from session_personnelle where sessionID = '$sessionID';";
-    $cartes = mysqli_fetch_row(mysqli_query($conn, $sql));
-    $carte1 = $cartes[0];
-    $carte2 = $cartes[1];
-}
